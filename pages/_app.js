e8dc3617cee853345abaaf5a42be2ae96720c45e
Derryck RS <50745340+16N1K4@users.noreply.github.com>
2021-01-22 16:58:44 +0800

    Added service worker registration function
    Disclaimer: doesn't currently work

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
 import Router from 'next/router';
+import {useEffect} from 'react';
 import NProgress from 'nprogress'; //nprogress module
 import '../styles/nprogress.css'; //styles of nprogress
 import '../styles/layout-height.css';
@@ -9,6 +10,21 @@ Router.events.on('routeChangeComplete', () => NProgress.done());
 Router.events.on('routeChangeError', () => NProgress.done());
 
 function MyApp({ Component, pageProps }) {
+    useEffect(() => {
+    if("serviceWorker" in navigator) {
+      window.addEventListener("load", function () {
+       navigator.serviceWorker.register("/service-worker.js", {scope: '/'}).then(
+          function (registration) {
+            console.log("Service Worker registration successful with scope: ", registration.scope);
+          },
+          function (err) {
+            console.log("Service Worker registration failed: ", err);
+          }
+        );
+      });
+    }
+  }, [])
+
     return <Component {...pageProps} />
 }
 export default MyApp;
