a702dc7358c16e91463d387da33395ab36c8f36b
Derryck RS <50745340+16N1K4@users.noreply.github.com>
2021-03-27 06:40:22 +0800

    Added degree type column to table

--- components/unit-head/basic-info/education.js
+++ components/unit-head/basic-info/education.js
@@ -13,6 +13,7 @@ function Education() {
                         <tr>
                             <th>Institution/School</th>
                             <th>Degree/Certification</th>
+	    		    <th>Degree Type</th>
                             <th>Major/Specialization</th>
                             <th>Start Date</th>
                             <th>End Date</th>
@@ -23,6 +24,7 @@ function Education() {
                         <tr>
                             <td>University of the Philippines Diliman</td>
                             <td>Doctor of Philosophy in Computer Science</td>
+	    		    <td>PhD</td>
                             <td>None</td>
                             <td>2020-01-26</td>
                             <td>Present</td>
@@ -38,6 +40,7 @@ function Education() {
                         <tr>
                             <td>University of the Philippines Diliman</td>
                             <td>MS Computer Science</td>
+	    		    <td>MS</td>
                             <td>Biostat</td>
                             <td>2017-05-26</td>
                             <td>2020-01-25</td>
@@ -53,6 +56,7 @@ function Education() {
                         <tr>
                             <td>University of the Philippines Los Banos</td>
                             <td>BS Computer Science</td>
+	    		    <td>BS</td>
                             <td>Health Informatics</td>
                             <td>2010-09-06</td>
                             <td>2017-05-25</td>
