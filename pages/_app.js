eb1c4a6ea59ec081c21d22c563557d986427f34c
Derryck RS <50745340+16N1K4@users.noreply.github.com>
2021-04-27 08:55:53 +0800

    Removed service worker initialization

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,4 @@
 import Router from 'next/router';
-import {useEffect} from 'react';
 import NProgress from 'nprogress'; //nprogress module
 import '../styles/nprogress.css'; //styles of nprogress
 import '../styles/layout-height.css';
@@ -10,21 +9,6 @@ Router.events.on('routeChangeComplete', () => NProgress.done());
 Router.events.on('routeChangeError', () => NProgress.done());
 
 function MyApp({ Component, pageProps }) {
-    useEffect(() => {
-    if("serviceWorker" in navigator) {
-      window.addEventListener("load", function () {
-       navigator.serviceWorker.register("/service-worker.js", {scope: '/'}).then(
-          function (registration) {
-            console.log("Service Worker registration successful with scope: ", registration.scope);
-          },
-          function (err) {
-            console.log("Service Worker registration failed: ", err);
-          }
-        );
-      });
-    }
-  }, [])
-
     return <Component {...pageProps} />
 }
 export default MyApp;
