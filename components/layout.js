5990a6f879bd50ac5e0b2c931a555f4fc22fec9d
Derryck RS <50745340+16N1K4@users.noreply.github.com>
2021-05-10 04:21:16 +0800

    Installed Bootstrap Icons via CDN

--- components/layout.js
+++ components/layout.js
@@ -11,6 +11,7 @@ function Layout(props) {
                 <title>DPSM-QA-PORTAL</title>
                 {/* <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous"></link> */}
                 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" />
+		<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css" />
                 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
                 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
                 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
@@ -24,9 +25,6 @@ function Layout(props) {
                     </div>
                 </div>
 	    </div>
-            {/* <Footer /> */}
-		
-		
         </div>
     )
   }
