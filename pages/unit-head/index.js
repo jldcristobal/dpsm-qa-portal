c46f53d4c6ec8ac757af059f98dab6961da5462a
Derryck RS <50745340+16N1K4@users.noreply.github.com>
2021-05-20 10:31:14 +0800

    Added Attained Degrees tab

--- pages/unit-head/index.js
+++ pages/unit-head/index.js
@@ -1,6 +1,7 @@
 import LayoutUnitHead from '../../components/layout-unit-head'
 import AccomplishmentCount from '../../components/unit-head/dashboard/accomplishment-count/accomplishment-count'
 import EmploymentStatus from '../../components/unit-head/dashboard/employment-status/employment-status'
+import DegreeCount from '../../components/unit-head/dashboard/degree/degree'
 
 function Dashboard() {
     return (
@@ -11,12 +12,14 @@ function Dashboard() {
 					<a className="nav-item nav-link" id="accomplishment-count-tab" data-toggle="tab" href="#accomplishment-count" role="tab" aria-controls="accomplishment-count" aria-selected="false">Accomplishment Count</a>
 					<a className="nav-item nav-link" id="employment-status-tab" data-toggle="tab" href="#employment-status" role="tab" aria-controls="employment-status" aria-selected="false">Employment Status</a>
 					<a className="nav-item nav-link" id="SET-score-tab" data-toggle="tab" href="#SET-score" role="tab" aria-controls="SET-score" aria-selected="false">Outstanding SET scores</a>
+					<a className="nav-item nav-link" id="degree-tab" data-toggle="tab" href="#degree" role="tab" aria-controls="degree" aria-selected="false">Attained Degrees</a>
             			</div>
             		</nav>
 	    		<div className="tab-content" id="nav-tabContent">
 	    			<div className="tab-pane fade show active" id="accomplishment-count" role="tabpanel" aria-labelledby="accomplishment-count-tab"><AccomplishmentCount /></div>
 				<div className="tab-pane fade" id="employment-status" role="tabpanel" aria-labelledby="employment-status-tab"><EmploymentStatus /></div>
 	    			<div className="tab-pane fade" id="SET-score" role="tabpanel" aria-labelledby="SET-score-tab">sup</div>
+				<div className="tab-pane fade" id="degree" role="tabpanel" aria-labelledby="degree-tab"><DegreeCount /></div>
             		</div>
                 
 			<style jsx>{`
