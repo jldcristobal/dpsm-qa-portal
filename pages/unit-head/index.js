ad87b454d85848bded88b8f1f6c9a3137bce3f3e
Derryck RS <50745340+16N1K4@users.noreply.github.com>
2021-05-20 08:15:09 +0800

    Changed dashboard contents
    Dashboard now has a navbar pertaining to different categories of analytics, the components of which are in the updated dashboard directory of components.

--- pages/unit-head/index.js
+++ pages/unit-head/index.js
@@ -1,48 +1,35 @@
 import LayoutUnitHead from '../../components/layout-unit-head'
-import AnalyticsTable from '../../components/unit-head/dashboard/dashboard_table'
-import DashboardGraph from '../../components/unit-head/dashboard/dashboard_graph'
+import AccomplishmentCount from '../../components/unit-head/dashboard/accomplishment-count/accomplishment-count'
 
 function Dashboard() {
     return (
         <LayoutUnitHead>
-            <div className="col-9">
-		<br />
-	<h3 align = "center">Accomplishment Count</h3>
-	<div className = "form-row">
-                    	<div className = "form-group col-md-4 required">
-				<label className = "control-label" htmlFor ="StartTimePeriod"> From  </label>
-                    		<input className = "form-control" type = "date" name = "StartTimePeriod" required />
-                	</div>
-
-			<div className = "form-group col-md-4 required">
-				<label className = "control-label" htmlFor ="EndTimePeriod"> To </label>
-                    		<input className = "form-control" type = "date" name = "EndTimePeriod" required />
-                	</div>
-	</div>
-
-	<button className = "btn btn-info"> Change Time Period</button>
-		<nav>
-            		<div className="nav nav-tabs nav-fill nav-justified" id="nav-tab" role="tablist">
-				<a className="nav-item nav-link" id="graph-tab" data-toggle="tab" href="#graph" role="tab" aria-controls="graph" aria-selected="false">Overview</a>
-				<a className="nav-item nav-link" id="table-tab" data-toggle="tab" href="#table" role="tab" aria-controls="table" aria-selected="false">Full List</a>
+		<div className="col-9">
+            		<nav>
+            			<div className="nav nav-tabs nav-fill nav-justified" id="nav-tab" role="tablist">
+					<a className="nav-item nav-link" id="accomplishment-count-tab" data-toggle="tab" href="#accomplishment-count" role="tab" aria-controls="accomplishment-count" aria-selected="false">Accomplishment Count</a>
+					<a className="nav-item nav-link" id="employment-status-tab" data-toggle="tab" href="#employment-status" role="tab" aria-controls="employment-status" aria-selected="false">Employment Status</a>
+					<a className="nav-item nav-link" id="SET-score-tab" data-toggle="tab" href="#SET-score" role="tab" aria-controls="SET-score" aria-selected="false">Outstanding SET scores</a>
+            			</div>
+            		</nav>
+	    		<div className="tab-content" id="nav-tabContent">
+	    			<div className="tab-pane fade show active" id="accomplishment-count" role="tabpanel" aria-labelledby="accomplishment-count-tab"><AccomplishmentCount /></div>
+				<div className="tab-pane fade" id="employment-status" role="tabpanel" aria-labelledby="employment-status-tab">sup</div>
+	    			<div className="tab-pane fade" id="SET-score" role="tabpanel" aria-labelledby="SET-score-tab">sup</div>
             		</div>
-            	</nav>
-	    	<div className="tab-content" id="nav-tabContent">
-	    		<div className="tab-pane fade show active" id="graph" role="tabpanel" aria-labelledby="graph=tab"><DashboardGraph /></div>
-	    		<div className="tab-pane fade" id="table" role="tabpanel" aria-labelledby="table-tab"><AnalyticsTable /></div>
-            	</div>
-                </div>
-	<style jsx>{`
-		a.nav-item:focus{
-			background-color:#78b6c2;
-		}
-		a.nav-item:hover{
-			background-color:#78b6c2;
-		}
-		a.active{
-			background-color:#78b6c2;
-		}
-	`}</style>
+                
+			<style jsx>{`
+				a.nav-item:focus{
+					background-color:#78b6c2;
+				}
+				a.nav-item:hover{
+					background-color:#78b6c2;
+				}
+				a.active{
+					background-color:#78b6c2;
+				}
+			`}</style>
+		</div>
         </LayoutUnitHead>
     )
   }
